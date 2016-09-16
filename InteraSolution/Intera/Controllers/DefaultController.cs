1b334f673b4cb4230002a694a6e865ec2134faa5
leomsoares <leonardo.soares3@fatec.sp.gov.br>
2016-09-16 15:38:06 -0300

    Telas

---
+++ InteraSolution/Intera/Controllers/DefaultController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intera.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult adicionarprojeto()
+        {
+            return View();
+        }
+
+        public ActionResult professores()
+        {
+            return View();
+        }
+
+        public ActionResult iniciacaocientifica()
+        {
+            return View();
+        }
+
+        public ActionResult projetos()
+        {
+            return View();
+        }
+
+        public ActionResult perfil()
+        {
+            return View();
+        }
+
+        public ActionResult grupo()
+        {
+            return View();
+        }
+    }
+}
