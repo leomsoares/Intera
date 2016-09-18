d4bea3475074044b97f8ae9330d185cd8022eabd
Luiz Felipe <Luiz Felipe@DESKTOP-94FBTGQ>
2016-09-18 14:08:26 -0300

    View Criar Projeto

--- InteraSolution/Intera/Controllers/DefaultController.cs
+++ InteraSolution/Intera/Controllers/DefaultController.cs
@@ -43,5 +43,9 @@ namespace Intera.Controllers
         {
             return View();
         }
+        public ActionResult criarprojeto()
+        {
+            return View();
+        }
     }
 }
