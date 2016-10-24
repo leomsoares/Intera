0b258cf02e00dd64359ad1273fabbef19f37ab16
Luizphila <luiz.cordeiro3@etec.sp.gov.br>
2016-10-24 20:13:12 -0200

    controller

---
+++ InteraSolution/Intera/Controllers/ProjectController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intera.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult addproject()
+        {
+            return View();
+        }
+
+        public ActionResult createproject()
+        {
+            return View();
+        }
+
+        public ActionResult seekproject()
+        {
+            return View();
+        }
+    }
+}
