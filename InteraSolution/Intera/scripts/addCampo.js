e3214a29a452576d8ba9fe4ccb9fca469a83221d
leomsoares <leonardo.soares3@fatec.sp.gov.br>
2016-10-31 23:53:54 -0200

    add input dinamicamente

---
+++ InteraSolution/Intera/scripts/addCampo.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    var input = '<label style="display: inline-block"><input class="form-control inputProject-js" type="text" name="foto[]" /> <a href="#" class="remove"> X</a></label>';
+    $("input[name='add']").click(function (e) {
+        $('#inputs_adicionais').append(input);
+    });
+
+    $('#inputs_adicionais').delegate('a', 'click', function (e) {
+        e.preventDefault();
+        $(this).parent('label').remove();
+    });
+
+});
